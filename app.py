8b378fbb726dd0adf052aa5ff05a1b50dbe0f19d
Tushar Mittal <chiragmittal.mittal@gmail.com>
2020-08-05 23:02:56 +0530

    Initial files

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, send_file
+import requests
+from bs4 import BeautifulSoup
+from PIL import Image
+
+import datetime
+from io import BytesIO
+import random
+
+
+app = Flask(__name__)
+
+def get_new_memes():
+    url = 'https://www.memedroid.com/memes/tag/programming'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'lxml')
+    divs = soup.find_all('div', class_='item-aux-container')
+    imgs = []
+    for div in divs:
+        img = div.find('img')['src']
+        if img.startswith('http') and img.endswith('jpeg'):
+            imgs.append(img)
+    return imgs
+
+
+def serve_pil_image(pil_img):
+    img_io = BytesIO()
+    pil_img.save(img_io, 'JPEG', quality=70)
+    img_io.seek(0)
+    return send_file(img_io, mimetype='image/jpeg')
+
+
+@app.route("/", methods=['GET'])
+def return_meme():
+    img_url = random.choice(get_new_memes())
+    res = requests.get(img_url, stream=True)
+    res.raw.decode_content = True
+    img = Image.open(res.raw)
+    return serve_pil_image(img)
